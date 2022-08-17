20a15dc59ff7518a655379340d05fda1346c08c5
Aymen Sekhri <39885103+AymenSe@users.noreply.github.com>
2022-08-17 09:51:08 +0100

    Remove repeated key in the kwargs dictionary

--- 2-Weakly-Supervised-Subtyping/utils/utils.py
+++ 2-Weakly-Supervised-Subtyping/utils/utils.py
@@ -56,7 +56,7 @@ def collate_features(batch):
 
 
 def get_simple_loader(dataset, batch_size=1, num_workers=1):
-	kwargs = {'num_workers': 4, 'pin_memory': False, 'num_workers': num_workers} if device.type == "cuda" else {}
+	kwargs = {'pin_memory': False, 'num_workers': num_workers} if device.type == "cuda" else {}
 	loader = DataLoader(dataset, batch_size=batch_size, sampler = sampler.SequentialSampler(dataset), collate_fn = collate_MIL, **kwargs)
 	return loader 
 
